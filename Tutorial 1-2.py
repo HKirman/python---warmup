a254e3f3fa805a7f60e3deecb2dcfb457b1a74fd
HKirman <kirmanhabibe@gmail.com>
2025-05-20 00:21:46 +0300

    İlk commit

---
+++ Tutorial 1-2.py
@@ -0,0 +1,20 @@
+print("Hello World !")
+
+#Variable and Data Types
+
+
+str "Hello"   #single line string
+    'Hello'   #single line string
+              #Multi line string 
+ '''  
+   MULTI      
+   LINE
+   STRING
+'''
+            
+int 10              #integer number
+float 2.5           #float number
+complex (10,5)      #Firt part is reel number , second part is imaginer number
+bool True False     #Boolean
+
+
---
+++ Tutorial 2.py
@@ -0,0 +1,18 @@
+print("10"+"10")        #represents a string and + perform concantenation
+print(10+10)            #repsesents the sum
+print(type(5))          #Return the type of the value in parantheses
+
+x=10
+y="Hello"
+z=5
+whatever = True
+
+print(x)            #prints the x
+print(y)            #prints the y
+print(whatever)     #prints the whatever
+print(x+z)          #prints the x+z
+
+a="10"      #define an integer as a string
+            # print(a/2) ; If you define an integer as a string and try to print it using a/4 in the print function, it will give you an error.
+a=int(a)
+print(a/2)  #The value of a was converted to an integer and used as an integer in the print function.
